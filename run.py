e35e3ed3d2b5a8104a0db1f395638505e908b863
JeffersonAguiar <jfdruida@gmail.com>
2024-01-25 23:07:02 +0000

    Feat: Refactor API endpoint and add debug prints

--- run.py
+++ run.py
@@ -2,21 +2,18 @@ import os
 import requests
 
 from flask import Flask, request, jsonify
-# from flask_cors import CORS  # Import the CORS module
 
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-# CORS(app)  # Enable CORS for your Flask app
 
 @app.route("/mykey", methods=["GET"])
 def get_key():
     apiKey = os.environ.get("API_KEY")
-
     lat = request.args.get("lat")
     lon = request.args.get("lon")
-
+    print(lat, lon)
     if lat and lon:
         url = f"https://api.openweathermap.org/data/2.5/weather?units=metric&{lat}&lon={lon}&appid={apiKey}"
     else:
@@ -24,10 +21,11 @@ def get_key():
         if not location:
             return jsonify({"error": "Missing required parameter: location"}), 400
         url = f"https://api.openweathermap.org/data/2.5/weather?units=metric&q={location}&appid={apiKey}"
-    
+    print(url)
     response = requests.get(url)
-
+    print(response.status_code)
     if response.status_code == 200:
+        print(response.json())
         return jsonify(response.json()), 200
     else:
         return jsonify({"error": "Invalid API key"}), response.status_code
@@ -36,6 +34,8 @@ def get_key():
 def get_home():
     sample_url1 = "https://weather-dh-d49c7aef68c1.herokuapp.com/mykey?location=London"
     sample_url2 = "https://weather-dh-d49c7aef68c1.herokuapp.com/mykey?lat=51.5074&lon=-0.1278"
+    print(sample_url1)
+    print(sample_url2)
     return jsonify({"message": "Welcome to the Weather API!"}), 200
 
 if __name__ == "__main__":  
