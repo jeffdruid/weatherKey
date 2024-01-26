45d323b7b99dbca6be6987123226f45586489b08
JeffersonAguiar <jfdruida@gmail.com>
2024-01-26 00:52:14 +0000

    Refactor get_data and get_info functions

--- run.py
+++ run.py
@@ -11,30 +11,36 @@ if os.path.exists("env.py"):
 
 @app.route('/myapi', methods=['GET'])
 def get_data():
-    api_key = os.environ.get("API_KEY") 
-    # Check if lat and lon parameters are provided
-    lat = request.args.get('lat')
-    lon = request.args.get('lon')
+    try:
+        api_key = os.environ.get("API_KEY") 
+        # Check if lat and lon parameters are provided
+        lat = request.args.get('lat')
+        lon = request.args.get('lon')
 
-    if lat and lon:
-        external_api_url = f'https://api.openweathermap.org/data/2.5/weather?units=metric&lat={lat}&lon={lon}&appid={api_key}'
-    else:
-        location = request.args.get('location')
-        if not location:
-            return jsonify({"error": "No location or coordinates provided"}), 400
-        external_api_url = f'https://api.openweathermap.org/data/2.5/weather?units=metric&q={location}&appid={api_key}'
+        if lat and lon:
+            external_api_url = f'https://api.openweathermap.org/data/2.5/weather?units=metric&lat={lat}&lon={lon}&appid={api_key}'
+        else:
+            location = request.args.get('location')
+            if not location:
+                return jsonify({"error": "No location or coordinates provided"}), 400
+            external_api_url = f'https://api.openweathermap.org/data/2.5/weather?units=metric&q={location}&appid={api_key}'
 
-    response = requests.get(external_api_url)
-    if response.status_code == 200:
-        return jsonify(response.json())
-    else:
-        return jsonify({"error": "Failed to fetch data from external API"}), response.status_code
+        response = requests.get(external_api_url)
+        if response.status_code == 200:
+            return jsonify(response.json())
+        else:
+            return jsonify({"error": "Failed to fetch data from external API"}), response.status_code
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/', methods=['GET'])
 def get_info():
-    sample_url1 = "https://weather-key-160275f00837.herokuapp.com/myapi?location=London"
-    sample_url2 = "https://weather-key-160275f00837.herokuapp.com/myapi?lat=51.5074&lon=-0.1278"
-    return f"Sample URL with location: <strong>{sample_url1}</strong><br><br>Sample URL with latitude and longitude: <strong>{sample_url2}</strong>"
+    try:
+        sample_url1 = "https://weather-key-160275f00837.herokuapp.com/myapi?location=London"
+        sample_url2 = "https://weather-key-160275f00837.herokuapp.com/myapi?lat=51.5074&lon=-0.1278"
+        return f"Sample URL with location: <strong>{sample_url1}</strong><br><br>Sample URL with latitude and longitude: <strong>{sample_url2}</strong>"
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(
